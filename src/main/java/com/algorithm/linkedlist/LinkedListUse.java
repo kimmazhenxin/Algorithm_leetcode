407607bd8ef8bd555e88a8d8f2918bdd8962f21b
mazhenxin <mazhenxin@xinye.com>
2021-04-13 10:32:55 +0800

    链表

--- src/main/java/com/algorithm/linkedlist/LinkedListUse.java
+++ src/main/java/com/algorithm/linkedlist/LinkedListUse.java
@@ -2,14 +2,16 @@ package com.algorithm.linkedlist;
 
 /**
  * @Author: kim
- * @Description: 链表的使用
+ * @Description: 链表的使用,增删改查
  * @Date: 10:06 2021/4/9
  * @Version: 1.0
  */
 public class LinkedListUse {
-    //头结点
+    // 记录链表头结点位置(链表一定要定义头节点)
     private Node head = null;
 
+    // 记录链表尾节点的位置(特例,视情况而定义)
+    private Node tail = null;
 
     /**
      * 获取链表的第k个节点(k从0开始)
@@ -17,24 +19,119 @@ public class LinkedListUse {
      * @return
      */
     public Node get(int k) {
-
+        // p用来遍历链表节点,初始化为head
+        Node p = head;
+        // 记录现在遍历到了第几个节点
+        int i = 0;
+        // 循环条件
+        while (null != p) {
+            if (i == k) {return p;}
+            p = p.next;
+            i++;
+        }
         return null;
     }
 
-
-    //查找
-    public Node find() {
-
+    /**
+     * 查找链表中的第一个值
+     * @param value
+     * @return
+     */
+    public Node find(int value) {
+        // p用来遍历链表节点,初始化为head
+        Node p = head;
+        // 循环条件
+        while (null != p) {
+            if (p.data == value) {return p;}
+            p = p.next;
+        }
         return null;
     }
 
-    //插入
-    public void insert() {
+    /**
+     * 往链表头部插入,这种操作将于输入的顺序相反,逆序
+     * @param value
+     */
+    public void insertHead(int value) {
+        Node newNode = new Node(value,null);
+        // 指向刚开始的头节点
+        newNode.next = head;
+        // 新的头结点指向刚插入的新节点
+        head = newNode;
+    }
 
+    /**
+     * 往链表尾部插入数据,顺序插入
+     * @param value
+     */
+    public void insertAtTail(int value) {
+        Node newNode = new Node(value, null);
+        Node p = head;
+        // 空链表(考虑特例情况)
+        if (head == null) {
+            head = newNode;
+        } else {
+            // 下面这段循环逻辑是为了找到链表的尾结点
+            while (null != p.next) {
+                p = p.next;
+            }
+            // 将插入的节点赋值给尾结点
+            p.next = newNode;
+        }
     }
 
 
-    //删除
+    /**
+     * 链表尾部插入优化一:在定义链表的时候,定义一个尾结点 tail
+     * @param value
+     */
+    public void insertAtTail2(int value) {
+        // 假设链表定义了tail节点
+        Node newNode = new Node(value, null);
+        // 空链表(考虑特例情况)
+        if (null == head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    /**
+     * 链表尾部插入优化二:引入虚拟节点,结合之前的tail指针,简化编程,统一逻辑
+     * 引入虚拟节点之后,在空链表中插入数据和非空链表中插入数据的逻辑一样,不需要要特殊处理在非空链表中插入数据的这一特殊情况了
+     * @param value
+     */
+    public void insertAtTail3(int value) {
+        // 引入虚拟节点,实际就是将head指向一个虚拟节点,它不存储任何数据
+        Node head = new Node();
+        // tail和head都指向这个虚拟节点
+        tail = head;
+        Node newNode = new Node(value, null);
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+
+    /**
+     * 在给定节点之后插入一个新节点
+     * @param value
+     */
+    public void insertAfter(Node p, int value) {
+        if (null == p) {
+            return;
+        }
+        Node newNode = new Node(value, null);
+        newNode.next = p.next;
+        p.next = newNode;
+    }
+
+
+
+
+
+    // 删除
     public void delete() {
 
     }
@@ -49,6 +146,9 @@ class Node {
     public int data;
     //存储下一个节点的引用(地址)
     public Node next;
+
+    public Node() {
+    }
 
     public Node(int data, Node next) {
         this.data = data;
